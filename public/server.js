bb2213619bc3848a8a3a9e63749ccda03ed56d44
suraj <jsuraj61@gmail.com>
2023-01-02 17:21:21 +0530

    serevr update

---
+++ public/server.js
@@ -0,0 +1,14 @@
+var express=require("express");
+var path=require("path");
+var http=require("http");
+var app=express();
+
+app.use(express.static,path.join(__dirname,'public'));
+
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname,'/index.html'));
+
+})
+app.listen(6000,function(){
+    console.log("running on port 6000");
+})
