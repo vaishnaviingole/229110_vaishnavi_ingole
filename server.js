a6e25a9e07240341f4a5bd9e1d3ee8f82471b515
suraj <jsuraj61@gmail.com>
2023-01-02 17:41:19 +0530

    docker update

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ var path=require("path");
 var http=require("http");
 var app=express();
 
-app.use(express.static,path.join(__dirname,'public'));
+// app.use(express.static,path.join(__dirname,'public'));
 
-app.get("/",function(req,res){
-    res.sendFile(path.join(__dirname,'/index.html'));
+// app.get("/",function(req,res){
+//     res.sendFile(path.join(__dirname,'/index.html'));
 
+
+// })
+app.get("/",function(req,resp){
+  resp.send("hello world")
 })
 app.listen(6000,function(){
     console.log("running on port 6000");
